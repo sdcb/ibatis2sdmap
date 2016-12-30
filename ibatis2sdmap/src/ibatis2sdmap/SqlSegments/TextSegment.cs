cc236f84f3a385db257b7e45a76f45d00b819e28
sdcb <sdflysha@qq.com>
2016-12-30 11:21:48 +0800

    add $value$ replace

--- ibatis2sdmap/src/ibatis2sdmap/SqlSegments/TextSegment.cs
+++ ibatis2sdmap/src/ibatis2sdmap/SqlSegments/TextSegment.cs
@@ -16,15 +16,15 @@ namespace ibatis2sdmap.SqlSegments
             Text = text.Value;
         }
 
-        private static Regex pregex = new Regex(@"#(\w+)#");
-        private static Regex uregex = new Regex(@"\$(\w+)\$");
+        private static Regex reg2 = new Regex(@"#(\w+)#");
+        private static Regex reg3 = new Regex(@"\$(\w+)\$");
 
         public override string Emit()
         {
-            return
-                uregex.Replace(
-                    pregex.Replace(Text, "@$1"), 
-                    $"#prop<$1>");
+            var rep1 = Text.Replace("$value$", "#val<>");
+            var rep2 = reg2.Replace(rep1, "@$1");
+            var rep3 = reg3.Replace(rep2, $"#prop<$1>");
+            return rep3;
         }
 
         public readonly static TextSegment Empty = new TextSegment(new XText(""));
