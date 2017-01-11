123d2097ed199a8243421a845dd07550b8ab4fd5
sdcb <sdflysha@qq.com>
2017-01-11 12:45:54 +0800

    switch to #prop<> for complex prop

--- ibatis2sdmap/src/ibatis2sdmap/SqlSegments/TextSegment.cs
+++ ibatis2sdmap/src/ibatis2sdmap/SqlSegments/TextSegment.cs
@@ -16,9 +16,10 @@ namespace ibatis2sdmap.SqlSegments
             Text = text.Value;
         }
 
-        private static Regex reg2 = new Regex(@"#(\w+(\.\w+)?)#");
+        private static Regex reg2 = new Regex(@"#(\w+)#");
         private static Regex reg3 = new Regex(@"\$(\w+)\$");
         private static Regex reg4 = new Regex(@"#\[\]\.(\w+)#");
+        private static Regex reg5 = new Regex(@"#(\w+(\.\w+)?)#");
 
         public override string Emit()
         {
@@ -28,7 +29,8 @@ namespace ibatis2sdmap.SqlSegments
             var rep2 = reg2.Replace(rep1, "@$1");
             var rep3 = reg3.Replace(rep2, $"#prop<$1>");
             var rep4 = reg4.Replace(rep3, $"@$1");
-            return rep4;
+            var rep5 = reg5.Replace(rep4, $"#prop<$1>");
+            return rep5;
         }
 
         public readonly static TextSegment Empty = new TextSegment(new XText(""));
